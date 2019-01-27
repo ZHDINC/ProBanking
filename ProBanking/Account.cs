44986360b8b634b7a6cf2edfd754d08115b7f8a3
Zachary Drew <ZHDINC@gmail.com>
2019-01-26 20:57:45 -0500

    First Version. Created a basic account class. Menu functions are still in Main and thinking of migrating those functions into a separate class.

---
+++ ProBanking/Account.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBanking
+{
+    class Account
+    {
+        public Account(string NameOnAccount, string AccountNumber, string hashtopass, double Balance, double InterestRate)
+        {
+            this.Balance = Balance;
+            this.InterestRate = InterestRate;
+            this.NameOnAccount = NameOnAccount;
+            this.AccountNumber = AccountNumber;
+            Password = hashtopass.GetHashCode();
+        }
+
+        public Account() { }
+
+        public double Balance { get; set; }
+        public double InterestRate { get; set; }
+        public string NameOnAccount { get; set; }
+        public string AccountNumber { get; set; }
+        private int Password { get; set; }
+
+        public double Deposit(double deposit)
+        {
+            double AfterDeposit = Balance + deposit;
+            Console.WriteLine("After depositing ${0}, your account value is now ${1}", deposit, AfterDeposit);
+            return Balance += deposit;
+        }
+
+        public double Credit(double credit)
+        {
+            double AfterCredit = Balance - credit;
+            if(AfterCredit < 0)
+            {
+                Console.WriteLine("You are overdrawn. Transaction declined.");
+                OverdraftFee(15);
+                return Balance;
+            }
+            else
+            {
+                Console.WriteLine("After crediting ${0}, your account value is now ${1}.", credit, AfterCredit);
+                return Balance -= credit;
+            }
+        }
+
+        private double OverdraftFee(double fee)
+        {
+            return Balance -= fee;
+        }
+
+        public void GetCurrentBalance()
+        {
+            Console.WriteLine("Your current balance is ${0}.", Balance);
+        }
+
+        public bool Login()
+        {
+            string username = "";
+            string password = "";
+            Console.WriteLine("Please login to access banking services.");
+            Console.Write("Username: ");
+            username = Console.ReadLine();
+            Console.Write("Password: ");
+            password = Console.ReadLine();
+            int hashedpass = password.GetHashCode();
+            if (Password == hashedpass)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProBanking/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBanking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Banking *****");
+            Console.Write("Create a name on the account: ");
+            string name = Console.ReadLine();
+            Console.Write("Create a password: ");
+            string password = Console.ReadLine();
+            Account newaccount = new Account(name, "935-4831", password, 500, 0.04);
+            
+            string loopcontinue = "YES";
+            while (loopcontinue == "YES")
+            {
+                if (newaccount.Login())
+                {
+                    MainMenu(newaccount);
+                }
+                else
+                {
+                    Console.WriteLine("Login Failure! You have entered an invalid password!");
+                }
+                Console.Write("Continue? :");
+                loopcontinue = Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        static void MainMenu(Account account)
+        {
+            bool loopvalue = true;
+            int accountoptions = 0;
+            int monetaryInput = 0;
+            string charInput = "n";
+            while (loopvalue == true)
+            {
+                Console.WriteLine("Please make a selection:\n[1] Check Balance [2] Deposit [3] Credit [4] Quit");
+                try
+                {
+                    accountoptions = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                switch (accountoptions)
+                {
+                    case 1:
+                        account.GetCurrentBalance();
+                        break;
+                    case 2:
+                        Console.Write("How much do you wish to deposit? : ");
+                        monetaryInput = Convert.ToInt32(Console.ReadLine());
+                        account.Deposit(monetaryInput);
+                        break;
+                    case 3:
+                        Console.Write("How much do you wish to withdraw? : ");
+                        monetaryInput = Convert.ToInt32(Console.ReadLine());
+                        account.Credit(monetaryInput);
+                        break;
+                    case 4:
+                        Console.Write("Do you wish to quit? (y/n): ");
+                        charInput = Console.ReadLine();
+                        if (charInput.ToUpper() == "N")
+                            break;
+                        else
+                            loopvalue = false;
+                        break;
+                    default:
+                        Console.WriteLine("Received invalid input.");
+                        break;
+                }
+            }
+        }
+    }
+}
